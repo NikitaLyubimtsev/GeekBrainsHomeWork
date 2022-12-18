3080668b96cfe3706c623a97945f5cd5680e635f
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-19 00:07:01 +0300

    Update Task4

--- HomeWork/Task4/Program.cs
+++ HomeWork/Task4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+Write("Введите третье число: ");
+int c = int.Parse(ReadLine()!);
+
+int max = a;
+
+if (b > max) {
+    max = b;
+} else if (c > max) {
+    max = c;
+}
+
+Write($"Максимальным из введённых является число: {max}");
