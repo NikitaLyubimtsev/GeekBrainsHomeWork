83b8727d5b237316917087ca5b7531b98cd9c0f5
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-19 00:09:59 +0300

    Update Task4

--- HomeWork/Task4/Program.cs
+++ HomeWork/Task4/Program.cs
@@ -11,10 +11,7 @@ int c = int.Parse(ReadLine()!);
 
 int max = a;
 
-if (b > max) {
-    max = b;
-} else if (c > max) {
-    max = c;
-}
+if (b > max) max = b;
+if (c > max) max = c;
 
 Write($"Максимальным из введённых является число: {max}");
