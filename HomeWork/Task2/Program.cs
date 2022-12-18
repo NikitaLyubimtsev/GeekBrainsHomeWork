7789bcabe64be1e48ffbcea407000210f535d0d5
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-18 23:56:39 +0300

    Update Task2

--- HomeWork/Task2/Program.cs
+++ HomeWork/Task2/Program.cs
@@ -1,13 +1,16 @@
 ﻿using static System.Console;
 
 WriteLine("Введите два числа: ");
-int a = int.Parse(ReadLine()!);
 
+int a = int.Parse(ReadLine()!);
 int b = int.Parse(ReadLine()!);
 int max = a;
+int min = a;
 
 if (b > max) {
     max = b;
+} else {
+    min = b;
 }
 
-WriteLine($"Максимальное, из введённых чисел: {max}");
+WriteLine($"Максимальное, из введённых чисел: {max}, минимальное {min}.");
