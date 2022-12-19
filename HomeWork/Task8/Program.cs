1f96d1193bd2a15c66eedcf2101842d044f0f4f5
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-19 23:43:50 +0300

    Update Task8

--- HomeWork/Task8/Program.cs
+++ HomeWork/Task8/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите любое число: ");
+int n = int.Parse(ReadLine()!);
+
+int count = 0;
+
+if (n > 1 || n % 2 != 0) {
+   n++;
+}
+
+while ((n - 2) > count) {
+    count += 2;
+    if (count == (n - 1) || count == (n - 2)) {
+        Write(count);
+    } else {
+        Write($"{count}, ");
+    }
+}
