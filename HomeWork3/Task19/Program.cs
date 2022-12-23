fba531d4e1dea1ea022e60040efcc82faccc9b96
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-23 16:55:23 +0300

    Task19

---
+++ HomeWork3/Task19/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите любое пятизначное число: ");
+int num = Convert.ToInt32(ReadLine());
+WriteLine(num < 9999 ? "Вы ввели не пятизначное число!" : ((num % 10) == (num / 10000) && (num % 100 / 10) == (num / 1000 % 10)) ? "Да" : "Нет");
