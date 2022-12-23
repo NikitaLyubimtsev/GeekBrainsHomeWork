3465711633f31676d9a2478f56f76414f4ad8391
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-23 16:04:33 +0300

    Added decision Task15

--- HomeWork2/Task15/Program.cs
+++ HomeWork2/Task15/Program.cs
@@ -1,2 +1,6 @@
 ﻿using static System.Console;
 Clear();
+
+Write("Введите любое число обозначающее день недели (от 1 до 7): ");
+int num = Convert.ToInt16(ReadLine());
+WriteLine(num > 7 ? $"Цифра {num} не обозначение не один из дней недели!" : num < 6 ? "Нет" : "Да");
