e55bc8889e42785b42daa01bec408faac61ec160
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-23 15:56:40 +0300

    Added decision Task13

--- HomeWork2/Task13/Program.cs
+++ HomeWork2/Task13/Program.cs
@@ -1,2 +1,6 @@
 ﻿using static System.Console;
 Clear();
+
+Write("Введите любое число: ");
+int num = Convert.ToInt32(ReadLine());
+WriteLine(num > 999 ? num / 100 % 10 : num < 99 ? "Третей цифры нет!" : num / 100);
