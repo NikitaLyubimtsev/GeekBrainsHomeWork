9b897733ceeae1dcca5d528cc806307d0b0a7526
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-23 17:45:13 +0300

    Added decision Task23

---
+++ HomeWork3/Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите любое число: ");
+if (int.TryParse(ReadLine(), out int n)) {
+    int num = 1;
+    while (num <= n) {
+        Write(num == n ? Math.Pow(num, 3) : $"{Math.Pow(num, 3)}, ");
+        num++;
+    } 
+} else {
+    WriteLine("Ошибка ввода!");
+}
