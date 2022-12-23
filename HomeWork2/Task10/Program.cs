99ccf3e862acdf27bd4993ed89d6782cf0f8b89c
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2022-12-23 15:30:25 +0300

    Decision Task10

--- HomeWork2/Task10/Program.cs
+++ HomeWork2/Task10/Program.cs
@@ -1,2 +1,6 @@
 ﻿using static System.Console;
 Clear();
+
+Write("Введите любое трёхзначное число: ");
+string num = ReadLine();
+WriteLine(num.Length != 3 ? "Вы наверное ошиблись и ввели не трёхзначное число!" : (Convert.ToInt32(num)%100/10));
