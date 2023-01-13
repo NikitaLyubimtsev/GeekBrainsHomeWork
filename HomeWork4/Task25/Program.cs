a19697bceaf2904da436fa08652d0d775a34e0b7
NikitaAlexandrovich <Lyubimtsevb.a@yandex.ru>
2023-01-13 09:53:53 +0300

    Added decision Task25

---
+++ HomeWork4/Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+
+
+using static System.Console;
+Clear();
+
+int DegreeOfNumber(int a, int b)
+{
+    int res = a;
+    for (int i = 0; i < (b - 1); i++) {
+
+        res = res * a;
+    }
+    return res;
+}
+WriteLine(DegreeOfNumber(3, 5));
