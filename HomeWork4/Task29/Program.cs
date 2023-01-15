a7b216de6f3cfd69ca9e41c24da116a4b9c7652c
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2023-01-15 20:02:22 +0300

    Added decision Task29

---
+++ HomeWork4/Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, реализующую метод, который формирует массив случайных целых чисел из 8 элементов и выводит их на экран.
+using static System.Console;
+Clear();
+
+void RandomNumArray(int len)
+{
+    Random rnd = new Random();
+    int[] res = new int[len];
+    for (int i = 0; i < len; i++) {
+        res[i] = rnd.Next(100);
+    }
+    for (int n = 0; n < res.Length; n++) {
+        Write(res[n]);
+        if ((len - 1) > n) Write(",");
+    }
+}
+
+Write("Введите длину массива: ");
+if (int.TryParse(ReadLine(), out int len)){}
+else Write("Ошибка ввода!");
+
+RandomNumArray(len);
