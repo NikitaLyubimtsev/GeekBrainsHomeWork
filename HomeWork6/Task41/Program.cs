8891089a74e1fb3e0caebfa711c2b73731cf7ae2
NikitaAlexandrovich <Lyubimtsevb.a@yandex.ru>
2023-01-23 12:11:41 +0300

    Added decision Task41

---
+++ HomeWork6/Task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using static System.Console;
+Clear();
+
+Write("Введите числа через запятую без пробелов: ");
+string inputNumbers = ReadLine()!;
+
+int[] StringToIntArrya(string value)
+{
+    return Array.ConvertAll(value.Split(","), s=>int.Parse(s));
+}
+
+int CountPositivNumbers(int[] valueArray)
+{
+    int count = 0;
+    foreach (int v in valueArray) {
+        if (v > 0) count++;
+    }
+    return count;
+}
+
+Write("Количество положительных чисел из введёных, равно: " + CountPositivNumbers(StringToIntArrya(inputNumbers)));
