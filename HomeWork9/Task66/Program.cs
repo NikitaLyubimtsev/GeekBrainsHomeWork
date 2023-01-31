11e672e1935e234a20a9958b2b922e470cca589b
NikitaAlexandrovich <lyubimtsevn.a@yandex.ru>
2023-01-31 14:25:21 +0300

    Added decision Task66

---
+++ HomeWork9/Task66/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.
+using static System.Console;
+Clear();
+
+
+WriteLine(SumToNumbers(1, 5));
+
+int SumToNumbers(int begin, int end)
+{
+    return (begin == end) ? begin : begin + SumToNumbers(begin + 1, end);
+}
